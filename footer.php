622b8facb02d01275d7831852c81338a01b48335
fabiantheblind <fabiantheblind@googlemail.com>
2013-01-10 22:34:19 +0100

    fiddeling with home added dashbord link to footer

--- footer.php
+++ footer.php
@@ -5,11 +5,15 @@
         <div class="span12">
             <footer class="foot">
                 <p>
-                    <a href="#">Impressum</a> |&nbsp;<a href="/netzwerk/kontakt/">Kontakt</a> | <a href="/netzwerk/newsletter/">Newsletter</a>
+                    <a href="#">Impressum</a> |&nbsp;<a href="<?php echo get_permalink( 14 ); ?>">Kontakt</a> | <a href="<?php echo get_permalink( 17 ); ?>">Newsletter</a>
+                    <?php
+                    if ( is_user_logged_in() ) {
+                     $url = admin_url();
+                    echo ' | <a href="' . $url . '""> Dashboard </a>';
+                 } ?>
                 </p>
             </footer>
         </div>
     </div>
 </div><!-- /container -->
 <?php wp_footer(); ?>
- 
