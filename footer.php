47dbe4a9d6be5e4e06fd3f4c847203338a388ee6
fabiantheblind <fabiantheblind@googlemail.com>
2013-01-10 16:22:41 +0100

    simple autopush

--- footer.php
+++ footer.php
@@ -1,20 +1,15 @@
- 
+
 <div class="container">
-
-       <hr>
-       <footer>
-         
-
-<div class="row">
-    <div class="span12">
-      <footer class="foot">
-        <p><a href="#">Impressum</a> | <a href="/netzwerk/kontakt/">Kontakt</a> | <a href="/netzwerk/newsletter/">Newsletter</a></p>
-      </footer>
+    <hr>
+    <div class="row">
+        <div class="span12">
+            <footer class="foot">
+                <p>
+                    <a href="#">Impressum</a> |&nbsp;<a href="/netzwerk/kontakt/">Kontakt</a> | <a href="/netzwerk/newsletter/">Newsletter</a>
+                </p>
+            </footer>
         </div>
     </div>
-    </div> <!-- /container -->
-
-    <?php wp_footer(); ?>
-  </footer>
-  </body>
-</html>
+</div><!-- /container -->
+<?php wp_footer(); ?>
+ 
