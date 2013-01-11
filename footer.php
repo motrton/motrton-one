3acbd2c328059732b616114e38a53bc986ed8413
fabiantheblind <fabiantheblind@googlemail.com>
2013-01-11 14:34:07 +0100

    added register form

--- footer.php
+++ footer.php
@@ -7,9 +7,17 @@
                 <p>
                     <a href="#">Impressum</a> |&nbsp;<a href="<?php echo get_permalink( 14 ); ?>">Kontakt</a> | <a href="<?php echo get_permalink( 17 ); ?>">Newsletter</a>
                     <?php
+                    $dashboardlink = admin_url();
+                    $loginlink = wp_login_url( get_permalink() );
+                    $registerlink = get_page_link(220);
                     if ( is_user_logged_in() ) {
-                     $url = admin_url();
-                    echo ' | <a href="' . $url . '""> Dashboard </a>';
+                    
+                    echo ' | <a href="' . $dashboardlink . '""> Dashboard <i class="icon-wrench"></i></a>'; 
+                 }else{
+                    
+                    echo ' | <a href="' . $loginlink . '""> Einloggen <i class="icon-signin"></i></a> | <a href="' . $registerlink . '""> Registrieren <i class="icon-user"></i></a>'; 
+
+                    
                  } ?>
                 </p>
             </footer>
